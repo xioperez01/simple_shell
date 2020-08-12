218590a52a08d63705ed6764b26450b45349a2b4
xioperez01 <angie.xioperez@gmail.com>
2020-08-12 19:31:27 +0000

    Initial header

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* libratries */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+
+/* prototypes */
+
+int prompt(char **argv);
+char *read_line(void);
+
+#endif
