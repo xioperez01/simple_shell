3e6842cae86417846c604e4a019dae8a38bf9434
xioperez01 <angie.xioperez@gmail.com>
2020-08-14 16:16:38 +0000

    Adding prototype and structure of stat function

--- shell.h
+++ shell.h
@@ -15,10 +15,29 @@
 #include <dirent.h>
 #include <signal.h>
 
+/* structure for stat function */
+
+struct stat {
+	dev_t     st_dev;     /* ID of device containing file */
+	ino_t     st_ino;     /* inode number */
+	mode_t    st_mode;    /* protection */
+	nlink_t   st_nlink;   /* number of hard links */
+	uid_t     st_uid;     /* user ID of owner */
+	gid_t     st_gid;     /* group ID of owner */
+	dev_t     st_rdev;    /* device ID (if special file) */
+	off_t     st_size;    /* total size, in bytes */
+	blksize_t st_blksize; /* blocksize for file system I/O */
+	blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
+	time_t    st_atime;   /* time of last access */
+	time_t    st_mtime;   /* time of last modification */
+	time_t    st_ctime;   /* time of last status change */
+};
+
 /* prototypes */
 
 char *read_line(void);
 char **split_line(char *input);
 int exec_commands(char **commands);
+int _stat(const char *phat, struct stat *buf);
 
 #endif
