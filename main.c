cf4f14d43a1e0593fa2d80e8f9c16fbb94695d6b
Martin <dev.martincorredor@gmail.com>
2020-08-14 20:27:12 +0000

    Adding stat function and printing message if file not found

--- main.c
+++ main.c
@@ -11,18 +11,30 @@ int main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv)
 	char *line = NULL;
 	char *line_converted;
 	char **commands;
-	int status;
+	int status, i;
+	struct stat description;
+	char s[] = "NOT FOUND";
 
 	do {
 		if (isatty(STDIN_FILENO))
 			write(1, "MAXIO~$ ", 8);
+
 		line = read_line();
 		printf("leyó bn esto: %s\n\n", line);
 		line_converted = convert_to_null(line);
 		printf("convirtió bn esto: %s\n\n", line_converted);
 		commands = split_line(line_converted);
 		printf("separó bn esto: %s %s %s\n\n", commands[0], commands[1], commands[2]);
-		status = exec_commands(commands);
+		if (stat(commands[0], &description) == 0)
+			status = exec_commands(commands);
+		else
+		{
+			for(i = 0; s[i] != '\0'; i++)
+				{
+					_putchar(s[i]);
+					_putchar('\n');
+				}
+		}
 		free(line);
 		free(commands);
 	} while (status);
