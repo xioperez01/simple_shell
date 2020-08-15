5bffc5c468db20a8a1ebb1e9b91fb29a431f4c1a
xioperez01 <angie.xioperez@gmail.com>
2020-08-15 21:37:24 +0000

    Adding function that compare two strings: _srtsrt

--- aux_func.c
+++ aux_func.c
@@ -11,3 +11,39 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ *_strstr - compare two string
+ *@haystack: input string
+ *@needle: string to compare
+ *Return: 0 on success or -1 if failed
+ */
+int _strstr(char *haystack, char *needle)
+{
+	int i = 0, j, x;
+
+	if (needle[0] == '\0')
+		return (-1);
+
+	if (haystack[0] == needle[0])
+	{
+		while (haystack[i] != '\0' && needle[i] != '\0')
+		{
+			x = 0, j = 0;
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == needle[j])
+				{
+					x++, j++;
+				}
+				else
+				{
+					return (-1);
+				}
+			}
+			return (0);
+		}
+		i++;
+	}
+	return (-1);
+}
